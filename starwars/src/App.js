35a4390a2de12ac5930b33d08a5e592f7ad05172
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-05-03 18:55:39 -0500

    I think I got everything to communicate!

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,38 +1,33 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Character from "./components/Character.js";
 
 const App = () => {
   const [data, setData] = useState([]);
-  useEffect(() => {
-    axios
-      .get(
-        "https://rickandmortyapi.com/api/"
-      )
-      .then((response) => {
-        console.log(response.data);
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  axios
+    .get("https://rickandmortyapi.com/api/")
+    .then((response) => {
+      console.log(response.data);
+      setData(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character />
     </div>
   );
-}
+};
 
 export default App;
 
-
-
-
 // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+// the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+// Fetch characters from the API in an effect hook. Remember, anytime you have a
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
