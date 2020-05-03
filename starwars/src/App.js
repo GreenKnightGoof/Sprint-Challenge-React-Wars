60adea55b057b174f5c7d0d43b61d388a7edf438
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-05-03 18:28:52 -0500

    Getting things set up

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://rickandmortyapi.com/api/"
+      )
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="App">
@@ -17,3 +26,13 @@ const App = () => {
 }
 
 export default App;
+
+
+
+
+// Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
