755fdac79651a78cac4a99e9d9d9b40ccd227ad0
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-05-03 20:55:37 -0500

    Commit for Sprint Retro

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,7 +5,7 @@ import Character from "./components/Character.js";
 
 const App = () => {
   axios
-    .get("https://rickandmortyapi.com/api")
+    .get("https://rickandmortyapi.com/api/character/")
     .then((response) => {
       console.log(response.data);
     })
@@ -15,8 +15,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      <Character />
+      <h1 className="Header">Characters From Rick and Morty!</h1>
+      <Character  />
     </div>
   );
 };
